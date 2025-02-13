4b9dc64869a55850e7406ae5627ed38c2512736d
DimitriGilbert <dimitri.gilbert@gmail.com>
2025-02-13 16:40:46 +0100

    [misc] dedicated sub domain

--- next.config.js
+++ next.config.js
@@ -7,8 +7,6 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
   output: "export",
-  basePath: process.env.NODE_ENV === 'production' ? '/TerMinable' : '', // Replace with your repository name
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/TerMinable/' : '', // Replace with your repository name
 };
 
 export default config;
